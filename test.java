ba2cb016cf881e53e3b3dac513fc9d09bd57c2cf
bigone-77 <taeill012gmailcom@skuniv.ac.kr>
2023-01-10 18:29:09 +0900

    java 파일 생성

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class test {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.print(str);
+    }
+}
