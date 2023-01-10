0f2ba31af2c13f8899b43c55251cbd3ead572466
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2023-01-10 18:47:20 +0900

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class test {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        System.out.print(str);
-    }
-}
